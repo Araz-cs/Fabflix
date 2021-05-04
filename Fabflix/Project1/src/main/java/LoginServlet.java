dc4751b02e9af952c28d7c8628319ffd3b743959
Araz-cs <52028482+Araz-cs@users.noreply.github.com>
2021-05-03 21:28:31 -0700

    added recaptcha

--- Fabflix/Project1/src/main/java/LoginServlet.java
+++ Fabflix/Project1/src/main/java/LoginServlet.java
@@ -16,6 +16,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 2L;
@@ -30,12 +39,35 @@ public class LoginServlet extends HttpServlet {
         }
     }
 
+    public String getServletInfo() {
+        return "Servlet connects to MySQL database and displays result of a SELECT";
+    }
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 
+        PrintWriter out = response.getWriter();
+
+        String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+        System.out.println("gRecaptchaResponse=" + gRecaptchaResponse);
+
+        // Verify reCAPTCHA
+        try {
+            recaptchaVerifyUtils.verify(gRecaptchaResponse);
+        } catch (Exception e) {
+            out.println("<html>");
+            out.println("<head><title>Error</title></head>");
+            out.println("<body>");
+            out.println("<p>recaptcha verification error</p>");
+            out.println("<p>" + e.getMessage() + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+
+            out.close();
+            return;
+        }
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -45,7 +77,7 @@ public class LoginServlet extends HttpServlet {
             Connection conn = dataSource.getConnection();
 
             //String query = "SELECT * FROM Customers c WHERE c.email="+username+" AND c.cPassword="+password ;
-            String query = "SELECT * FROM customers c WHERE c.email=? AND c.cPassword=?";
+            String query = "SELECT * FROM Customers c WHERE c.email=? AND c.cPassword=?";
 
             PreparedStatement statement = conn.prepareStatement(query);
 
@@ -84,4 +116,3 @@ public class LoginServlet extends HttpServlet {
 
     }
 }
-
---
+++ Fabflix/Project1/src/main/java/recaptchaConstants.java
@@ -0,0 +1,5 @@
+package main.java;
+
+public class recaptchaConstants {
+    public static final String SECRET_KEY ="6LehN8UaAAAAAAFBBE5xnOyyDOFwOiS5L8_JL19M";
+}
